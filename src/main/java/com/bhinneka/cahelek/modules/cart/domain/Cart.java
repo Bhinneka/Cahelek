ed2b67a3cc34cd46a13078e6593302bb265dbd17
wuriyanto <wuriyanto48@yahoo.co.id>
2019-02-26 07:09:17 +0700

    add cart domain model

---
+++ src/main/java/com/bhinneka/cahelek/modules/cart/domain/Cart.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2019 wuriyanto.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bhinneka.cahelek.modules.cart.domain;
+
+import com.bhinneka.cahelek.modules.product.domain.Product;
+import java.util.Map;
+
+/**
+ *
+ * @author wurianto
+ */
+public class Cart {
+
+    private Integer id;
+    private Map<Integer, Item> items;
+
+    public Cart() {
+    }
+
+    public Cart(Integer id, Map<Integer, Item> items) {
+        this.id = id;
+        this.items = items;
+    }
+
+    public void addOrUpdateItem(int itemId, Product product, int quantity) {
+        if (this.items.containsKey(itemId)) {
+            Item existingItem = this.items.get(itemId);
+            existingItem.update(quantity, product);
+        } else {
+            Item item = new Item(itemId, product, quantity);
+            this.items.put(item.getId(), item);
+        }
+
+    }
+    
+    public Item getItem(int itemId) {
+        Item item = null;
+        if (this.items.containsKey(itemId)) {
+            item = this.items.get(itemId);
+        }
+        return item;
+    }
+    
+    public Double getTotal() {
+        double total = 0.0;
+        for(Item v : this.items.values()) {
+            total += v.getSubTotal();
+        }
+        return total;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Map<Integer, Item> getItems() {
+        return items;
+    }
+
+}
---
+++ src/main/java/com/bhinneka/cahelek/modules/cart/domain/Item.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2019 wuriyanto.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bhinneka.cahelek.modules.cart.domain;
+
+import com.bhinneka.cahelek.modules.product.domain.Product;
+
+/**
+ *
+ * @author wurianto
+ */
+public class Item {
+    
+    private Integer id;
+    private Product product;
+    private Integer quantity;
+    private Double subTotal;
+
+    public Item() {
+        
+    }
+
+    public Item(Integer id, Product product, Integer quantity) {
+        this.id = id;
+        this.product = product;
+        this.quantity = quantity;
+        this.subTotal = quantity * product.getPrice();
+    }
+    
+    public void update(int quantity, Product product) {
+        this.quantity = quantity;
+        this.product = product;
+        this.subTotal = quantity * product.getPrice();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+
+    public Double getSubTotal() {
+        return subTotal;
+    }  
+
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id + ", product=" + product + ", quantity=" + quantity + ", subTotal=" + subTotal + '}';
+    }
+    
+}
