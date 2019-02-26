02a7904aede2d9105959ba95fd394ab8b354c6be
wuriyanto <wuriyanto48@yahoo.co.id>
2019-02-26 07:09:35 +0700

    add product domain model

---
+++ src/main/java/com/bhinneka/cahelek/modules/product/domain/Product.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bhinneka.cahelek.modules.product.domain;
+
+/**
+ *
+ * @author wurianto
+ */
+public class Product {
+
+    private Integer id;
+    private String name;
+    private Double price;
+
+    public Product() {
+        
+    }
+
+    public Product(Integer id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name=" + name + ", price=" + price + '}';
+    }
+
+}
